8d7fb8d23b96acf1d71ee139a40198129ba0dd35
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2022-02-25 23:38:55 +1100

    compile script

--- compile.sh
+++ compile.sh
@@ -5,11 +5,11 @@ set -x
 export PATH=/home/hasindu/grpcshit/bin:$PATH
 DIR=$(pwd)
 rm -r $HOME/grpc/examples/cpp/minknow_api
-rm -r $HOME/grpc/examples/proto
-cp $HOME/Optimised-Read-Until-API/minknow_api $HOME/grpc/examples/cpp/
-cp $HOME/Optimised-Read-Until-API/proto $HOME/grpc/examples/
+rm -r $HOME/grpc/examples/protos
+cp -r $HOME/Optimised-Read-Until-API/minknow_api $HOME/grpc/examples/cpp/
+cp -r $HOME/Optimised-Read-Until-API/proto $HOME/grpc/examples/
 cd $HOME/grpc/examples/cpp/minknow_api/cmake/build
 cmake ../../
-make
+make -j
 cp read_until $DIR/
 cd $DIR
